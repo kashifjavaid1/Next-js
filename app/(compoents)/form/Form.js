4def286f768dfdf30923ab1259d2080e93edebe8
kashifjavaid1 <gmdcf797@gmail.com>
2023-12-23 20:18:57 +0500

    form handler

--- app/(compoents)/form/Form.js
+++ app/(compoents)/form/Form.js
@@ -5,15 +5,19 @@ import { useState } from "react"
 export default function Form() {
     const [name,setName]=useState('')
     const [phone,setPhone]=useState();
+    const [email,setEmail]=useState();
   return (
    <>
    <form>
 Name:<input className="pl-2 border border-red-400" type="text" placeholder="Enter Name"  onChange={(e)=>setName(e.target.value)}/> <br />
 Phone Number: <input  className="pl-2 my-2 border border-red-400"  type="number" placeholder="number" onChange={(e)=>setPhone(e.target.value)} />
+
+Email: <input  className="pl-2 my-2 border border-red-400"  type="text" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
    </form>
    <br />
    <span>Name:{name}</span>
    <h1>Phone:{phone}</h1>
+   <h1>Email:{email}</h1>
    </>
   )
 }
