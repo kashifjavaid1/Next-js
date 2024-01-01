cce1e1a5defaa8ce93b540537b91e711db6459b7
kashifjavaid1 <gmdcf797@gmail.com>
2024-01-01 20:46:50 +0500

    commit

--- app/currency/page.js
+++ app/currency/page.js
@@ -3,7 +3,7 @@ import useCurrency from "./(hooks)/hooks"
 export default function page() {
   return (
     <>
-    <h1></h1>
+    <h1>Hooks</h1>
     <button>click</button>
     <h1 className="py-4 text-center text-4xl  ">Currency app</h1>
     <useCurrency/>
