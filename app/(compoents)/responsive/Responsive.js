adbcc8fe091c1f6031196d8f2254f9ec0664eee5
kashifjavaid1 <gmdcf797@gmail.com>
2024-01-06 18:52:26 +0500

    cart

---
+++ app/(compoents)/responsive/Responsive.js
@@ -0,0 +1,10 @@
+
+export default function Responsive() {
+  return (
+    <>
+    <div className="bg-red-800 md:bg-green-900 lg:bg-blue-950">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est sed ipsa expedita voluptas ullam tempore delectus suscipit vero maxime.</p>
+    </div>
+    </>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 // import Fecth from './(compoents)/datafecth/datafecth'
 // import Form from './(compoents)/form/Form'
 import Card from './(compoents)/card/card'
+import Responsive from './(compoents)/responsive/Responsive'
 export default function Home() {
   return (
     <>
@@ -18,12 +19,13 @@ export default function Home() {
    <Header title="Second Compoents" sorce="vercel.svg"/>
    <Header title="second"/> */}
 
-   <div className=' mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-4 md:mx-auto lg:gap-4  '>
+   {/* <div className=' mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-4 md:mx-auto lg:gap-4  '>
    <Card/>
    <Card/>
    <Card/>
    <Card/>
-   </div>
+   </div> */}
+   <Responsive/>
    </>
   )
 }
