0d385ee993e49c42d729cc0d5bdde4383683871b
kashifjavaid1 <gmdcf797@gmail.com>
2024-01-07 18:31:06 +0500

    navbar

--- app/(compoents)/responsive/Responsive.js
+++ app/(compoents)/responsive/Responsive.js
@@ -1,10 +1,29 @@
+import Link from "next/link";
 
 export default function Responsive() {
   return (
     <>
-    <div className="bg-red-800 md:bg-green-900 lg:bg-blue-950">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est sed ipsa expedita voluptas ullam tempore delectus suscipit vero maxime.</p>
-    </div>
+      <div className="flex justify-between text-white m-0 bg-slate-600">
+        <div>
+          <span>Logo</span>
+        </div>
+        <div className="py-2">
+          <ul className="flex px-4 p-4" >
+           <Link href='/'><li>Home</li></Link>
+           <Link href='/'><li>About</li></Link>
+           <Link href='/'><li>Contact</li></Link>
+           <Link href='/'><li>Service</li></Link>
+          </ul>
+        </div>
+        <div>
+          <span>
+          <button>Login</button>
+          <button>Sign In</button>
+          </span>
+
+
+        </div>
+      </div>
     </>
   )
 }
