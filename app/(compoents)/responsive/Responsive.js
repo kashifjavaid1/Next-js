8090ce6bb57cee19858d20baae5b66d11cb1bac6
kashifjavaid1 <gmdcf797@gmail.com>
2024-01-11 18:54:40 +0500

    commit

--- app/(compoents)/responsive/Responsive.js
+++ app/(compoents)/responsive/Responsive.js
@@ -17,7 +17,6 @@ export default function Responsive() {
         </div>
         <div>
           <span>
-            <button></button>
           <button>Login</button>
           <button>Sign In</button>
           </span>
